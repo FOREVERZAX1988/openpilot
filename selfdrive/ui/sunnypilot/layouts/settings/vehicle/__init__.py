491514a78a3f31ea6b09b1aa74e6712250e6508f
amoghmunikote <amogh.munikote@gmail.com>
2025-12-02 17:02:12 -0800

    Merging sunnypilot into ui improvements

--- selfdrive/ui/sunnypilot/layouts/settings/vehicle/__init__.py
+++ selfdrive/ui/sunnypilot/layouts/settings/vehicle/__init__.py
@@ -9,6 +9,10 @@ from openpilot.system.ui.widgets import Widget
 from openpilot.system.ui.widgets.list_view import ButtonAction
 from openpilot.system.ui.widgets.scroller_tici import Scroller
 
+<<<<<<< HEAD
+from openpilot.selfdrive.ui.sunnypilot.layouts.settings.vehicle.brands.factory import BrandSettingsFactory
+=======
+>>>>>>> 9560e30a5df614093c2d192d98755e2080aa6db1
 from openpilot.selfdrive.ui.sunnypilot.layouts.settings.vehicle.platform_selector import PlatformSelector, LegendWidget
 from openpilot.selfdrive.ui.ui_state import ui_state
 from openpilot.system.ui.sunnypilot.widgets.list_view import ListItemSP
@@ -17,6 +21,12 @@ from openpilot.system.ui.sunnypilot.widgets.list_view import ListItemSP
 class VehicleLayout(Widget):
   def __init__(self):
     super().__init__()
+<<<<<<< HEAD
+    self._brand_settings = None
+    self._brand_items = []
+    self._current_brand = None
+=======
+>>>>>>> 9560e30a5df614093c2d192d98755e2080aa6db1
     self._platform_selector = PlatformSelector(self._update_brand_settings)
 
     self._vehicle_item = ListItemSP(title=self._platform_selector.text, action_item=ButtonAction(text=tr("Select")),
@@ -27,14 +37,42 @@ class VehicleLayout(Widget):
     self.items = [self._vehicle_item, self._legend_widget]
     self._scroller = Scroller(self.items, line_separator=True, spacing=0)
 
+<<<<<<< HEAD
+  @staticmethod
+  def get_brand():
+    if bundle := ui_state.params.get("CarPlatformBundle"):
+      return bundle.get("brand", "")
+    elif ui_state.CP and ui_state.CP.carFingerprint != "MOCK":
+      return ui_state.CP.brand
+    return ""
+
+=======
+>>>>>>> 9560e30a5df614093c2d192d98755e2080aa6db1
   def _update_brand_settings(self):
     self._vehicle_item._title = self._platform_selector.text
     self._vehicle_item.title_color = self._platform_selector.color
     vehicle_text = tr("Remove") if ui_state.params.get("CarPlatformBundle") else tr("Select")
     self._vehicle_item.action_item.set_text(vehicle_text)
 
+<<<<<<< HEAD
+    brand = self.get_brand()
+    if brand != self._current_brand:
+      self._current_brand = brand
+      self._brand_settings = BrandSettingsFactory.create_brand_settings(brand)
+      self._brand_items = self._brand_settings.items if self._brand_settings else []
+
+      self.items = [self._vehicle_item, self._legend_widget] + self._brand_items
+      self._scroller = Scroller(self.items, line_separator=True, spacing=0)
+
   def _update_state(self):
     self._update_brand_settings()
+    if self._brand_settings:
+      self._brand_settings.update_settings()
+    self._platform_selector.refresh()
+=======
+  def _update_state(self):
+    self._update_brand_settings()
+>>>>>>> 9560e30a5df614093c2d192d98755e2080aa6db1
 
   def _render(self, rect):
     self._scroller.render(rect)
