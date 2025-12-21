472d9fc273d95861023dce6fb87992dd74ed7ad7
FOREVERZAX1988 <329086252@qq.com>
2025-12-22 00:26:39 +0800

    add button for change server comma or konik:fix bugs

--- common/api/comma_connect.py
+++ common/api/comma_connect.py
@@ -2,23 +2,10 @@ import os
 
 from openpilot.common.api.base import BaseApi
 
-from openpilot.common.params import Params
-
-def get_api_host():
-    params = Params()
-    try:
-        server_type = params.get("ServerType", "konik")
-    except Exception:
-        server_type = "konik"  # 异常时使用默认值
-    if server_type == "comma":
-        return os.getenv('API_HOST', 'https://api.commadotai.com')
-    else:
-        return os.getenv('API_HOST', 'https://api.konik.ai')
-
-API_HOST = get_api_host()
+API_HOST = os.getenv('API_HOST', 'https://api.konik.ai')
 
 
 class CommaConnectApi(BaseApi):
-    def __init__(self, dongle_id):
-        super().__init__(dongle_id, API_HOST)
-        self.user_agent = "openpilot-"
+  def __init__(self, dongle_id):
+    super().__init__(dongle_id, API_HOST)
+    self.user_agent = "openpilot-"
--- tools/lib/api.py
+++ tools/lib/api.py
@@ -1,21 +1,7 @@
 import os
 import requests
 from requests.adapters import HTTPAdapter, Retry
-# 修改api.py中的API_HOST定义
-# 原代码:
-# API_HOST = os.getenv('API_HOST', 'https://api.commadotai.com')
-# 替换为:
-from openpilot.common.params import Params
-
-def get_api_host():
-    params = Params()
-    server_type = params.get("ServerType", "konik")
-    if server_type == "comma":
-        return os.getenv('API_HOST', 'https://api.commadotai.com')
-    else:
-        return os.getenv('API_HOST', 'https://api.konik.ai')
-
-API_HOST = get_api_host()
+API_HOST = os.getenv('API_HOST', 'https://api.konik.ai')
 
 # TODO: this should be merged into common.api
 
