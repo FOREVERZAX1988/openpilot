753be68110dd7c9bf7e0767a82506323f32e8590
FOREVERZAX1988 <329086252@qq.com>
2025-12-22 00:08:57 +0800

    add button for change server comma or konik:fix bugs

--- common/api/comma_connect.py
+++ common/api/comma_connect.py
@@ -6,7 +6,10 @@ from openpilot.common.params import Params
 
 def get_api_host():
     params = Params()
-    server_type = params.get("ServerType", "konik")
+    try:
+        server_type = params.get("ServerType", "konik")
+    except Exception:
+        server_type = "konik"  # 异常时使用默认值
     if server_type == "comma":
         return os.getenv('API_HOST', 'https://api.commadotai.com')
     else:
@@ -16,6 +19,6 @@ API_HOST = get_api_host()
 
 
 class CommaConnectApi(BaseApi):
-  def __init__(self, dongle_id):
-    super().__init__(dongle_id, API_HOST)
-    self.user_agent = "openpilot-"
+    def __init__(self, dongle_id):
+        super().__init__(dongle_id, API_HOST)
+        self.user_agent = "openpilot-"
