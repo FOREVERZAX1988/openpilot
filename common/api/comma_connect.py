bc5f13c5b8c585a965e2f9bd19629fb6be50ad94
FOREVERZAX1988 <329086252@qq.com>
2025-12-22 22:26:25 +0800

    add button select server

--- common/api/comma_connect.py
+++ common/api/comma_connect.py
@@ -5,9 +5,10 @@ from openpilot.common.api.base import BaseApi
 from openpilot.common.params import Params
 
 #API_HOST = os.getenv('API_HOST', 'https://api.konik.ai')
-API_HOST = "https://api.konik.ai" if Params().get_bool("UseKonikServer") else "https://api.commadotai.com"
+#API_HOST = "https://api.konik.ai" if Params().get_bool("UseKonikServer") else "https://api.commadotai.com"
 
 class CommaConnectApi(BaseApi):
   def __init__(self, dongle_id):
+    API_HOST = "https://api.konik.ai" if Params().get_bool("UseKonikServer") else "https://api.commadotai.com"
     super().__init__(dongle_id, API_HOST)
     self.user_agent = "openpilot-"
